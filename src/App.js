b7a8193557c4ef68309df1a4348c597fcd4a2fc6
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-11 15:48:50 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookShow from './components/BookShow';
+import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
+    const createBook = (title) => {
+        const updatedBooks = [
+            ...books,
+            { id: Math.round(Math.random() * 9999), title }
+        ];
+        setBooks(updatedBooks);
+    };
 
-    return <div>App</div>;
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    return (
+        <div className="app">
+            <BookList books={books} onDelete={deleteBookById} />
+            <BookCreate onCreate={createBook} />
+        </div>
+    );
 }
 
 export default App;
